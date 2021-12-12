75ef00c085cf0a9224a436046ffcd1103bf564ad
Rebecca Rossman <47158686+rrossman25@users.noreply.github.com>
2021-12-12 17:01:22 -0500

    all articles index added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+    # show list of articles
+    def index
+        @articles = Article.all
+
+    end
+    # show individual article
     def show
         # byebug
         @article = Article.find(params[:id])
