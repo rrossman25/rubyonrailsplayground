58fb806a0bb8472ebf720619c10b3e0115a73234
Rebecca Rossman <47158686+rrossman25@users.noreply.github.com>
2021-12-12 19:02:30 -0500

    add rest of crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,48 @@
 class ArticlesController < ApplicationController
-    # show list of articles
-    def index
-        @articles = Article.all
-
+  # show list of articles
+  def index
+    @articles = Article.all
+  end
+  # show individual article
+  def show
+    # byebug
+    @article = Article.find(params[:id])
+  end
+  # show form for new article
+  def new
+    @article = Article.new
+  end
+  # edit an article
+  def edit
+    @article = Article.find(params[:id])
+  end
+  # create a new article in db
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article created successfully"
+      redirect_to @article
+    else
+      render 'new'
     end
-    # show individual article
-    def show
-        # byebug
-        @article = Article.find(params[:id])
+  end
+  
+  # update the article in db
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
+  # destroy the article in db
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article deleted successfully"
+    redirect_to articles_path
+  end
 end
