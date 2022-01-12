d0c313371dfb74197030d9af16d089cd027fe6bd
Rebecca Rossman <47158686+rrossman25@users.noreply.github.com>
2022-01-11 19:13:41 -0500

    clean up forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :loggedin?
+  helper_method :logged_in?
   def logged_in?
     !!current_user
   end
