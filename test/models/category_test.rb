98decc611ba9c8b7e371850b1b52f7a7ebb600e3
Rebecca Rossman <47158686+rrossman25@users.noreply.github.com>
2022-01-12 15:34:08 -0500

    add more model unit tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,11 +2,34 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
+  def setup
+      @category = Category.new(name: "Sports")
+  end
+
   test "category should be valid" do
-    @category = Category.new(name: "Sports")
     assert @category.valid?
   end
 
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
 
-  
+  end
+
+  test "name should be unique" do
+    @category.save
+    @newcategory = Category.new(name: "Sports")
+    assert_not @newcategory.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aaa"
+    assert_not @category.valid?  
+  end
+
 end
