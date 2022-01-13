c96dec28b7c8629cdda36d2b156990b90029769e
Rebecca Rossman <47158686+rrossman25@users.noreply.github.com>
2022-01-12 22:49:38 -0500

    add cat tags and cat show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
   # show form for new article
   def new
     @article = Article.new
+    @categories = Category.all
   end
   # edit an article
   def edit
@@ -54,7 +55,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
